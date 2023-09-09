96d724e5a8280df65b84dc40deff1b29b7ea468e
mayamarshel <144174991+mayamarshel@users.noreply.github.com>
2023-09-08 20:23:14 -0600

    added script

--- experiments/drawing/template-p5-project/js/script.js
+++ experiments/drawing/template-p5-project/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Drawing Shapes and Colors
+ * Maya Marshel
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -20,6 +20,12 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(200, 200);
+
+    background(200, 200, 200);
+
+    rectMode(CENTER)
+    rect(100, 100, 100, 100);
 
 }
 
