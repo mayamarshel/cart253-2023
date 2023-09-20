17ad027651754dde762d2ab2480bb833361eefde
mayamarshel <144174991+mayamarshel@users.noreply.github.com>
2023-09-19 21:28:59 -0600

    Finished I lIke to Move it Assignment

--- activities/i-like-to-move-it/template-p5-project/js/script.js
+++ activities/i-like-to-move-it/template-p5-project/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * I Like to Move It
+ * Maya Marshel
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -11,6 +11,36 @@
 /**
  * Description of preload
 */
+
+//variable for background
+let backgroundShade = 0;
+
+// Establish square object
+let box ={
+    x: 400,
+    y: 600,
+    size: 200,
+    stop: 70,
+    fill: 0
+};
+
+//Establish circle object
+let ball ={
+    x: 500,
+    y: 0, 
+    height: 500,
+    speed: 5,
+    shrink: 0.5
+};
+
+//establish rectangle
+let rectangle ={
+    x: 0, 
+    y: 100,
+    w: 100, 
+    h: 100,
+};
+
 function preload() {
 
 }
@@ -21,6 +51,9 @@ function preload() {
 */
 function setup() {
 
+    createCanvas(1000, 1000);
+
+    
 }
 
 
@@ -29,4 +62,29 @@ function setup() {
 */
 function draw() {
 
+//make so ball leaves no trail and make background change color fully across the screen with mouse input
+    backgroundShade = mouseX, 50, 20;
+    backgroundShade = map(mouseX, 0, width, 0, 255);
+    background(backgroundShade);
+    
+// make circle fall and stop inside box
+    ball.y = ball.y + ball.speed;
+
+    ball.y = constrain(ball.y, 0, box.y + box.stop);
+
+//make ball shrink and stop in box
+    ball.height = ball.height - ball.shrink;
+    ball.height = constrain(ball.height, 0, 100);
+    
+//spawn objects
+    ellipse(ball.x, ball.y, ball.height);
+    square(box.x, box.y, box.size);     
+
+//spawn rectangle and make move with mouse
+   rect(mouseX, rectangle.y, rectangle.w, rectangle.h);
+
+
+  
+
+   
 }
