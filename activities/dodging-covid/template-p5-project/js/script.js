48853f10edb9cc2c05f89cdd035c2b6c491b31ad
mayamarshel <144174991+mayamarshel@users.noreply.github.com>
2023-09-25 18:44:07 -0600

    added in Covid and Player objects to dodging covid

--- activities/dodging-covid/template-p5-project/js/script.js
+++ activities/dodging-covid/template-p5-project/js/script.js
@@ -1,32 +1,69 @@
 /**
- * Title of Project
- * Author Name
+ * Dodging Covid
+ * Maya Marshel
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
  */
 
-"use strict";
+let covid19 = {
+    x: 0, 
+    y: 250, 
+    size: 100, 
+    vx: 0, 
+    vy: 0, 
+    speed: 5,
+    fill: {
+        r: 255, 
+        g: 0, 
+        b: 0
+    }
+};
 
-/**
- * Description of preload
-*/
-function preload() {
+let player = {
+    x: 250, 
+    y: 250, 
+    size: 100, 
+    fill: 255
+};
 
-}
 
-
-/**
- * Description of setup
-*/
 function setup() {
+// creating canvas and setting spawning conditions for objects
+    createCanvas(windowWidth, windowHeight);
 
+    covid19.y = random(0, height);
+    covid19.vx = covid19.speed; 
 }
 
 
-/**
- * Description of draw()
-*/
 function draw() {
+    background(0);
 
+// adds velocity to movement (don't really need it for y but its a good thing to have)
+    covid19.x = covid19.x + covid19.vx;
+    covid19.y = covid19.y + covid19.vy;
+
+// resets position after reaching right side of screen
+    if (covid19.x > width) {
+        covid19.x = 0;
+        covid19.y = random(0, height);
+    }
+
+    player.x = mouseX;
+    player.y = mouseY;
+
+// create local distance variable, don't need it later on
+    let distance = dist(player.x, player.y, covid19.x, covid19.y);
+    if (distance < player.size/2 + covid19.size/2) {
+        noLoop();
+    }
+
+
+// fills covid bubble with predetermined rgb using an object inside of an object
+    fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+    ellipse(covid19.x, covid19.y, covid19.size);
+
+    fill(player.fill);
+    ellipse(player.x, player.y, player.size);
 }
