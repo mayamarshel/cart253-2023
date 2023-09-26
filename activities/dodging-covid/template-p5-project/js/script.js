11b4ffcc05460dd0c259da2a1a8888e0f4c4b35d
mayamarshel <144174991+mayamarshel@users.noreply.github.com>
2023-09-26 17:54:59 -0600

    added modifiers to covid game

--- activities/dodging-covid/template-p5-project/js/script.js
+++ activities/dodging-covid/template-p5-project/js/script.js
@@ -19,15 +19,20 @@ let covid19 = {
         b: 0
     }
 };
-
 let player = {
     x: 250, 
     y: 250, 
     size: 100, 
-    fill: 255
+    fill: 255,
 };
 
 let staticAmount = 1000;
+let covidImage;
+
+function preload(){
+    covidImage = loadImage("assets/images/covidtransp.jpg");
+}
+
 
 
 function setup() {
@@ -39,8 +44,14 @@ function setup() {
 }
 
 
+
+
+
 function draw() {
-    background(0);
+//set background and covid image infront of background, I know the picture looks ridiculous but it made me laugh so i kept it.
+//i hate when pictures say transparent but then aren't
+    background(110, 0, 30);
+    image(covidImage, windowWidth/3, windowHeight/3);
 
     for (let i = 0; i < staticAmount; i++) {
         let x = random(0, width);
@@ -48,10 +59,11 @@ function draw() {
         stroke(255);
         point(x, y);
      }
-
 // adds velocity to movement (don't really need it for y but its a good thing to have)
     covid19.x = covid19.x + covid19.vx;
     covid19.y = covid19.y + covid19.vy;
+
+
 
 // resets position after reaching right side of screen
     if (covid19.x > width) {
@@ -59,8 +71,6 @@ function draw() {
         covid19.y = random(0, height);
     }
 
-    player.x = mouseX;
-    player.y = mouseY;
 
 // create local distance variable, don't need it later on
     let distance = dist(player.x, player.y, covid19.x, covid19.y);
@@ -70,10 +80,26 @@ function draw() {
     }
 
 
+
 // fills covid bubble with predetermined rgb using an object inside of an object
     fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
     ellipse(covid19.x, covid19.y, covid19.size);
 
     fill(player.fill);
     ellipse(player.x, player.y, player.size);
+
+// change covid color to purple when it hits half way and then turn back to red when it respawns
+if (covid19.x > windowWidth/2){
+    covid19.fill.b = 255}
+else if (covid19.x === 0){
+    covid19.fill.b = 0};
+
 }
+
+// makes player move by pressing the mouse
+function mousePressed(){
+    player.x = mouseX;
+    player.y = mouseY;
+    }
+    
+
