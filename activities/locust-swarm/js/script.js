685f8052b94e44e2d7ad895ff2fe0b88c7ca052b
mayamarshel <144174991+mayamarshel@users.noreply.github.com>
2023-11-02 19:30:56 -0600

    Locust Swarm: added some screens
    need to:
    - assign title screens to states
    - change look of characters
    - add press to starts function

--- activities/locust-swarm/js/script.js
+++ activities/locust-swarm/js/script.js
@@ -1,12 +1,13 @@
 
-// need to figure out mouse pressed funciton
-
+//working on: assigning titles to states
+//changing look of locusts and maybe wheat and bee
+//create click to play function
 
 "use strict";
 
 
 let crops = {
-    // array and total flower number
+  // array and total wheat number
   wheatArray: [],
   totalWheat: 20,
 
@@ -22,6 +23,7 @@ let crops = {
   }
 };
 
+state = 'title'
 
 function setup() {
   createCanvas(600, 600);
@@ -33,9 +35,8 @@ function setup() {
     let wheat = new Wheat();
     crops.wheatArray.push(wheat);
   }
-
-  let bee = new Bee();
   
+  //sets random positions for each locust in array and counts up to total given
   for (let i = 0; i < crops.totalLocusts; i++) {
     //sets x and y to random 
     let x = random(0, width);
@@ -44,7 +45,7 @@ function setup() {
     //creates new local variable with class locust description inside (with x and y parameters)
     let locust = new Locust(x, y)
 
-    //adds new bee to bee array
+    //adds new locust to locust array
     crops.locusts.push(locust);
   }
   
@@ -53,17 +54,15 @@ function setup() {
 
 
 function draw() {
-    //respawns background each frame
-    background(crops.grassColor.r, crops.grassColor.g, crops.grassColor.b);
+  //respawns background each frame
+  background(crops.grassColor.r, crops.grassColor.g, crops.grassColor.b);
     
-    //creates Bee character and maps to user mouse if Bee is alive
+  //creates Bee character 
   let bee = new Bee;
 
 
-  if (bee.alive){
-   bee.display(mouseX, mouseY);
-  }
-
+  //displays the Bee character and puts locust class into killLocust function so as loop runs it will check each locust
+  //for overlap and if so will fire function
   for (let i = 0; i < crops.locusts.length; i++) {
    let locust = crops.locusts[i];
    if (bee.alive){
@@ -73,24 +72,22 @@ function draw() {
   }
 
 
-    for (let i = 0; i < crops.locusts.length; i++) {
-      //creates local variable that stores the specific bee in the array
-      // i is the index value of the bee in question
+  for (let i = 0; i < crops.locusts.length; i++) {
+      //creates local variable that stores the specific locust in the array
+      // i is the index value of the locust in question
       let locust = crops.locusts[i];
 
-      //calls the methods if the bee is alive
+      //calls the methods if the bee is alive/active
       if (locust.alive){
         locust.move();
-        
-        //bee will try and pollinate every flower
         //j is used because i has been used as index argument in this function already
-        //calls the tryToPollinate function with flower as the argument AFTER setting the
-        //flower local variable to mean a certain index of flower inside the flower array
+        //calls the killCrops function with wheat as the argument AFTER setting the
+        //wheat local variable to mean a certain index of wheat inside the wheat array
         for (let j = 0; j < crops.wheatArray.length; j++) {
           let wheat = crops.wheatArray[j];
           locust.killCrops(wheat);
         }
-        
+        //only displays locust if alive
         locust.display();
         }
       }
@@ -99,19 +96,69 @@ function draw() {
     //displays all the wheat stalks in the wheat array
     for (let i = 0; i < crops.wheatArray.length; i++) {
       let wheat = crops.wheatArray[i];
-      let bee = Bee;
       if (wheat.alive) {
         wheat.display(); 
       }
-      if (bee.alive === true){
-        bee.display(mouseX, mouseY);
-        console.log('there is a bee')
   
-  }
-    }
+    }  
+
+}
 
 
-    
+//SCREENS
+
+function locustsWin(){
+  state = 'locustsWin';
+  
+  push();
+  textSize(55);
+  fill(200, 0, 0);
+  textAlign(CENTER, CENTER);
+  text(`LOCUSTS WIN`, 350, 250);
+  pop();
+
+}  
+
+function beeWins(){
+  state = 'beeWins';
+  
+  push();
+  textSize(55);
+  fill(200, 0, 0);
+  textAlign(CENTER, CENTER);
+  text(`You saved the crops!`, 350, 250);
+  pop();
+
+}  
+
+function title(){
+  state = 'title';
+  
+  push();
+  textSize(55);
+  fill(200, 0, 0);
+  textAlign(CENTER, CENTER);
+  text(`Save the Crops`, 350, 250);
+  pop();
+
+  push();
+  textSize(30);
+  fill(200, 0, 0);
+  textAlign(CENTER, CENTER);
+  text(`Press ENTER to begin`, 350, 150);
+  pop();
+
+}  
+
+function gameScreen(){
+  state = 'beeWins';
+  
+  push();
+  textSize(55);
+  fill(200, 0, 0);
+  textAlign(CENTER, CENTER);
+  text(`You saved the crops!`, 350, 250);
+  pop();
 
 }
       
